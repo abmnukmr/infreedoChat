b8e52cbb5db24cce7bc7b90dda8186cd33d7cb52
manyu <manyu@manyus-MacBook-Pro.local>
2019-09-11 16:41:06 +0530

    Updates

---
+++ precache-manifest.29ab12efed01669b0d614065ab9dc4da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47383179260e370fd864e95872357ea6",
+    "url": "/infreedo/index.html"
+  },
+  {
+    "revision": "0357ba32d2a7b15c671b",
+    "url": "/infreedo/static/css/main.04296bb4.chunk.css"
+  },
+  {
+    "revision": "25093716d3a80b8980e1",
+    "url": "/infreedo/static/js/2.f597701d.chunk.js"
+  },
+  {
+    "revision": "0357ba32d2a7b15c671b",
+    "url": "/infreedo/static/js/main.9c7d540b.chunk.js"
+  },
+  {
+    "revision": "3e6bbf08f44ca8a8b28d",
+    "url": "/infreedo/static/js/runtime~main.4d68d24b.js"
+  }
+]);
