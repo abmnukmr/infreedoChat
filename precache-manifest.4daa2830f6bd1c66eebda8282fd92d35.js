2749e563b20f96ba61bce557d03c9b82a862b59f
manyu <manyu@manyus-MacBook-Pro.local>
2019-09-12 22:18:03 +0530

    Updates

--- precache-manifest.4daa2830f6bd1c66eebda8282fd92d35.js
+++ precache-manifest.4daa2830f6bd1c66eebda8282fd92d35.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "47652264e06b319a963f0240245bd14f",
+    "revision": "28a7dce4b7c91234384c24c15ba9e5c0",
     "url": "/infreedoChat/index.html"
   },
   {
-    "revision": "9647316667be7f1a283c",
+    "revision": "1eb53078402e7a0ea09f",
     "url": "/infreedoChat/static/css/main.6abef9ab.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/infreedoChat/static/js/2.2508bb11.chunk.js"
   },
   {
-    "revision": "9647316667be7f1a283c",
-    "url": "/infreedoChat/static/js/main.287ef133.chunk.js"
+    "revision": "1eb53078402e7a0ea09f",
+    "url": "/infreedoChat/static/js/main.80ac4d56.chunk.js"
   },
   {
     "revision": "b2f80c9de0862794575b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/infreedoChat/precache-manifest.36da4e3ced35392a6fdba69358d25357.js"
+  "/infreedoChat/precache-manifest.4daa2830f6bd1c66eebda8282fd92d35.js"
 );
 
 self.addEventListener('message', (event) => {
